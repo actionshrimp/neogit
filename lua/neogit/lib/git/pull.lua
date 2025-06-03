d80798e79221f4c68246120705e50a828d04e86b
Dave Aitken <dave.aitken@gmail.com>
2025-06-03 08:41:45 +0100

    add more timing info

--- lua/neogit/lib/git/pull.lua
+++ lua/neogit/lib/git/pull.lua
@@ -1,5 +1,6 @@
 local git = require("neogit.lib.git")
 local util = require("neogit.lib.util")
+local logger = require("neogit.logger")
 
 ---@class NeogitGitPull
 local M = {}
@@ -11,7 +12,19 @@ function M.pull_interactive(remote, branch, args)
 end
 
 local function update_unpulled(state)
+  local function elapsed_time(start_time)
+    return (vim.loop.hrtime() - start_time) / 1e6
+  end
+
+  local total_start = vim.loop.hrtime()
+  local last_time = total_start
+
+  logger.debug("[update_unpulled]: begin")
+
   local status = git.branch.status()
+  local current = vim.loop.hrtime()
+  logger.debug(("[update_unpulled]: git.branch.status() complete (%.2fms)"):format(elapsed_time(last_time)))
+  last_time = current
 
   state.upstream.unpulled.items = {}
   state.pushRemote.unpulled.items = {}
@@ -20,18 +33,31 @@ local function update_unpulled(state)
     return
   end
 
+  logger.debug("[update_unpulled]: status.upstream start")
   if status.upstream then
     state.upstream.unpulled.items =
       util.filter_map(git.log.list({ "..@{upstream}" }, nil, {}, true), git.log.present_commit)
   end
+  current = vim.loop.hrtime()
+  logger.debug(("[update_unpulled]: status.upstream complete (%.2fms)"):format(elapsed_time(last_time)))
+  last_time = current
 
+  logger.debug("[update_unpulled]: pushRemote_ref() start")
   local pushRemote = git.branch.pushRemote_ref()
+  current = vim.loop.hrtime()
+  logger.debug(("[update_unpulled]: pushRemote_ref() complete (%.2fms)"):format(elapsed_time(last_time)))
+  last_time = current
+
+  logger.debug("[update_unpulled]: pushRemote start")
   if pushRemote then
     state.pushRemote.unpulled.items = util.filter_map(
       git.log.list({ string.format("..%s", pushRemote) }, nil, {}, true),
       git.log.present_commit
     )
   end
+  current = vim.loop.hrtime()
+  logger.debug(("[update_unpulled]: pushRemote complete (%.2fms)"):format(elapsed_time(last_time)))
+  logger.debug(("[update_unpulled]: complete (%.2fms total)"):format(elapsed_time(total_start)))
 end
 
 function M.register(meta)
